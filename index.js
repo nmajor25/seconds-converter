6327d6ff5af73fb735017daa5e798b151e95170f
Daniel Dugovic <dandydand@gmail.com>
2018-09-23 09:09:07 -0500

    Fix compile error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (number, unit) {
   let d, h, m, s
 
   if (isNaN(number)) {
-    throw new TypeError('Value sent to seconds-converter must be a number.')
+    throw new TypeError("Value sent to seconds-converter must be a number.")
   }
 
   if (unit === 'sec' || unit === 'seconds') {
@@ -10,7 +10,7 @@ module.exports = function (number, unit) {
   } else if (unit === 'ms' || unit === 'milliseconds' || !unit) {
     s = Math.floor(number / 1000)
   } else {
-    throw new TypeError('Unit must be 'sec' or 'ms'')
+    throw new TypeError("Unit must be 'sec' or 'ms'")
   }
 
   m = Math.floor(s / 60)
